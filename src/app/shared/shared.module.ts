a1d9242aa1ddbd3ea33457394f6c0977175d4282
Allan Sham <allansham@hotmail.com>
2020-02-28 23:06:36 +0000

    Import a bunch of Angular material modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const components = [];
-const modules = [CommonModule, MatButtonModule];
+const modules = [
+  CommonModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatListModule,
+  MatIconModule,
+  MatToolbarModule,
+];
 
 @NgModule({
   declarations: [...components],
