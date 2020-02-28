2e15f0f186856d093aae28ab383783ddc4c2f9b7
Allan Sham <allansham@hotmail.com>
2020-02-28 23:09:01 +0000

    Create a side drawer component for list of cars

---
+++ src/app/main-app/car-drawer/car-drawer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-car-drawer',
+  templateUrl: './car-drawer.component.html',
+  styleUrls: ['./car-drawer.component.scss']
+})
+export class CarDrawerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
