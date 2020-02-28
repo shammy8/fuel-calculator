c079a7d09c9cc9a6a32f6683f774ed60de84da72
Allan Sham <allansham@hotmail.com>
2020-02-28 23:20:31 +0000

    Add add-car component + put it in the router

--- src/app/main-app/main-app-routing.module.ts
+++ src/app/main-app/main-app-routing.module.ts
@@ -2,8 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MainAppComponent } from './main-app.component';
+import { AddCarComponent } from './add-car/add-car.component';
 
-const routes: Routes = [{ path: '', component: MainAppComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainAppComponent,
+    children: [{ path: 'add', component: AddCarComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/main-app/main-app.module.ts
+++ src/app/main-app/main-app.module.ts
@@ -5,9 +5,10 @@ import { MainAppRoutingModule } from './main-app-routing.module';
 import { MainAppComponent } from './main-app.component';
 import { CarDrawerComponent } from './car-drawer/car-drawer.component';
 import { SharedModule } from '../shared/shared.module';
+import { AddCarComponent } from './add-car/add-car.component';
 
 @NgModule({
-  declarations: [MainAppComponent, CarDrawerComponent],
+  declarations: [MainAppComponent, CarDrawerComponent, AddCarComponent],
   imports: [CommonModule, SharedModule, MainAppRoutingModule],
 })
 export class MainAppModule {}
