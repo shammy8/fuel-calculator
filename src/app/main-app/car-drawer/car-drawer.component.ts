163311d64ba0457c3c9f3efa3ed01a52c2700309
Allan Sham <allansham@hotmail.com>
2020-03-02 19:21:58 +0000

    Make the car drawer responsive

--- src/app/main-app/car-drawer/car-drawer.component.ts
+++ src/app/main-app/car-drawer/car-drawer.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-car-drawer',
@@ -11,10 +14,22 @@ import { AngularFirestore } from '@angular/fire/firestore';
 export class CarDrawerComponent implements OnInit {
   cars$;
 
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(['(max-width: 780px)'])
+    .pipe(
+      map(result => {
+        return result.matches;
+      }),
+      shareReplay()
+    );
+
+  @ViewChild('sidenav') drawer;
+
   constructor(
     private router: Router,
     private afs: AngularFirestore,
-    private afAuth: AngularFireAuth
+    private afAuth: AngularFireAuth,
+    private breakpointObserver: BreakpointObserver
   ) {}
 
   ngOnInit(): void {
@@ -26,9 +41,19 @@ export class CarDrawerComponent implements OnInit {
 
   addCar() {
     this.router.navigate(['add']);
+    this.handleCloseDrawer();
   }
 
-  signOut() {
+  signOut(): void {
     this.afAuth.auth.signOut().then(() => this.router.navigate(['login']));
+  }
+
+  handleCloseDrawer(): void {
+    let isHandset: boolean;
+    this.isHandset$.subscribe(res => (isHandset = res));
+
+    if (isHandset) {
+      this.drawer.close();
+    }
   }
 }
