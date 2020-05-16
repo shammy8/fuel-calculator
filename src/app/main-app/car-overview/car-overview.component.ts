d37cc219087feea44c1bbc79dfc0244218ad2a18
Allan Sham <allansham@hotmail.com>
2020-05-16 11:40:26 +0100

    Pass the car details into car overview component

--- src/app/main-app/car-overview/car-overview.component.ts
+++ src/app/main-app/car-overview/car-overview.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Car } from '../Car.model';
 
 @Component({
   selector: 'app-car-overview',
   templateUrl: './car-overview.component.html',
-  styleUrls: ['./car-overview.component.scss']
+  styleUrls: ['./car-overview.component.scss'],
 })
 export class CarOverviewComponent implements OnInit {
+  @Input() carDetails: Car;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/main-app/dashboard.component.ts
+++ src/app/main-app/dashboard.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Car } from './Car.model';
+import { DatabaseService } from './database.service';
 
 @Component({
   selector: 'app-main-app',
-  template: ` <app-car-overview></app-car-overview> `,
+  template: `
+    <div *ngIf="cars$ | async as cars">
+      <app-car-overview
+        *ngFor="let car of cars"
+        [carDetails]="car"
+      ></app-car-overview>
+    </div>
+  `,
   styles: [],
 })
 export class DashboardComponent implements OnInit {
-  constructor() {}
+  cars$: Observable<Car[]> = this.databaseService.cars$;
+
+  constructor(private databaseService: DatabaseService) {}
 
   ngOnInit(): void {}
 }
