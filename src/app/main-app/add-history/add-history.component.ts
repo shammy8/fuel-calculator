718b7a7ea9226ca2e0d04c5f59cd90df1613240e
Allan Sham <allansham@hotmail.com>
2020-05-21 23:11:52 +0100

    Create the form for adding fuel

--- src/app/main-app/add-history/add-history.component.ts
+++ src/app/main-app/add-history/add-history.component.ts
@@ -4,6 +4,7 @@ import {
   MAT_BOTTOM_SHEET_DATA,
 } from '@angular/material/bottom-sheet';
 import { Car } from '../Car.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-history',
@@ -11,13 +12,32 @@ import { Car } from '../Car.model';
   styleUrls: ['./add-history.component.scss'],
 })
 export class AddHistoryComponent implements OnInit {
+  addFuelForm: FormGroup;
+
   constructor(
+    private fb: FormBuilder,
     private bottomSheetRef: MatBottomSheetRef<AddHistoryComponent>,
     @Inject(MAT_BOTTOM_SHEET_DATA) public carDetails: Car
   ) {}
 
   ngOnInit(): void {
-    console.log(this.carDetails);
-    // this.bottomSheetRef.dismiss('hi');
+    this.addFuelForm = this.fb.group({
+      mileage: [this.carDetails.latestHistory.mileage, [Validators.required]],
+      volume: [null, [Validators.required]],
+      cost: [null, [Validators.required]],
+      date: [new Date(), [Validators.required]],
+      comments: [''],
+    });
+  }
+
+  addFuel() {}
+
+  onReset() {
+    this.addFuelForm.reset();
+  }
+
+  onCancel(event: MouseEvent) {
+    this.bottomSheetRef.dismiss();
+    event.preventDefault(); // copying the docs
   }
 }
