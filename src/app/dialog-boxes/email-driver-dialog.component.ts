ccec235e41ab125fa6177a2217cf6475e7e39929
Allan Sham <allansham@hotmail.com>
2020-08-16 10:50:42 +0100

    Disable Email Driver button until a response is returned

--- src/app/dialog-boxes/email-driver-dialog.component.ts
+++ src/app/dialog-boxes/email-driver-dialog.component.ts
@@ -1,22 +1,28 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, ViewChild } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { User } from 'firebase';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { Car } from '../main-app/Car.model';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-email-driver-dialog',
   template: `
     <h1 mat-dialog-title>Driver doesn't exist</h1>
     <div mat-dialog-content>
-      The driver you want to add has not sign up to use this app. Click "Email
-      Driver" if you want to send the driver an email. Or "Cancel" to notify the
-      driver yourself.
+      {{ data.email }} has not sign up to use this app. Click "Email Driver" if
+      you want to send an email to {{ data.email }} to recommend this app. Or
+      click "Cancel" to notify the driver yourself.
     </div>
     <div class="buttons" mat-dialog-actions>
-      <button mat-raised-button color="primary" (click)="onEmailDriver()">
+      <button
+        mat-raised-button
+        color="primary"
+        (click)="onEmailDriver()"
+        #emailDriverButton
+      >
         Email Driver
       </button>
       <button mat-button mat-dialog-close>
@@ -35,8 +41,11 @@ export class EmailDriverDialogComponent {
     public data: { email: string; carDetails: Car; user: User }
   ) {}
 
+  @ViewChild('emailDriverButton') emailDriverButton: MatButton;
+
   async onEmailDriver() {
     try {
+      this.emailDriverButton.disabled = true;
       const res = await this.afs.collection('mail').add({
         to: this.data.email,
         template: {
@@ -51,6 +60,7 @@ export class EmailDriverDialogComponent {
       });
       this.dialogRef.close();
     } catch (err) {
+      this.emailDriverButton.disabled = false;
       this.snackBar.open(`Error: ${err}`, 'Close', {
         duration: 5000,
       });
